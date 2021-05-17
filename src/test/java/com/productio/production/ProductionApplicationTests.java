143a17a335b5b55d545dabfc49747e96cfc5805d
SKULLCRUSHER957 <sjoerd.halmans@gmail.com>
2021-05-17 10:44:51 +0200

    initialized project + added dockerfile

---
+++ src/test/java/com/productio/production/ProductionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.productio.production;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
