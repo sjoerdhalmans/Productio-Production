18f2bfc455bcfaf9f77b14b680ed589416086410
SKULLCRUSHER957 <sjoerd.halmans@gmail.com>
2021-05-17 11:01:12 +0200

    Commit to test discord connection - Travis loves you too Scar <3.

--- src/main/java/com/productio/production/ProductionApplication.java
+++ src/main/java/com/productio/production/ProductionApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ProductionApplication {
-//starts project
 	public static void main(String[] args) {
 		SpringApplication.run(ProductionApplication.class, args);
 	}
